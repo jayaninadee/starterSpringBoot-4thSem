1756c8c47daef5e4b18263b206caed05939c5880
jayaninadee <jayaninadeeshaa@gmail.com>
2020-10-31 15:46:39 +0530

    First Commit for project

---
+++ src/test/java/lk/ijse/day4/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.day4.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
